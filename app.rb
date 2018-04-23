e4cf290793ac809f4f7e19207f046e0a075f02c8
Shun Yao <ysmiracle@hotmail.com>
2018-04-23 02:29:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! showgun"
+  get '/' do
+    "Welcome to your app!!!! Testing shotgun!"
   end
 
 end
